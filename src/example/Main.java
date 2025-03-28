2ca71608bc6f09d56e401c900d6c012db6afcab8
elahee <elahe.nejatollahi@gmail.com>
2025-03-28 19:24:38 +0330

    check main

--- src/example/Main.java
+++ src/example/Main.java
@@ -5,50 +5,17 @@ import db.exception.EntityNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        Human[] humans = {
-                new Human("Gholi"),
-                new Human("Jamshid"),
-                new Human("Akbar"),
-        };
+        Human ali = new Human("Ali");
+        Database.add(ali);
 
-        System.out.println("#### Test add method ####");
-
-        for (Human h : humans) {
-            System.out.println("Adding " + h.name + " to the database.");
-            Database.add(h);
-        }
-
-        for (Human h : humans) {
-            System.out.println("Id of \"" + h.name + "\" is " + h.id + ".");
-        }
-
-        System.out.println();
-        System.out.println("#### Test get method ####");
-
-        int gholiId = humans[0].id;
-        Human gholi = (Human) Database.get(gholiId);
-
-        System.out.println("successfully got " + gholi.name + " from the database.");
-
-        System.out.println();
-        System.out.println("#### Test update method ####");
-
-        gholi.name = "Gholi Mohammadi";
-        Database.update(gholi);
-
-        Human gholiAgain = (Human) Database.get(gholiId);
-        System.out.println("Updated name: \"" + gholiAgain.name + "\".");
-
-        System.out.println();
-        System.out.println("#### Test delete method ####");
-
-        int jamshidId = humans[1].id;
-        Database.delete(jamshidId);
+        ali.name = "Ali Hosseini";
 
         try {
-            Human jamshid = (Human) Database.get(jamshidId);
+            Human aliFromTheDatabase = (Human) Database.get(ali.id);
+            System.out.println("ali's name in the database: " + aliFromTheDatabase.name);
+            // خروجی باید "Ali" باشد نه "Ali Hosseini"
         } catch (EntityNotFoundException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
